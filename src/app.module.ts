f4b6b5ce42f94842319434c61113f7a8a0a17cda
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-02-22 20:22:27 -0300

    feat: created connection with mongodb, and import schema for the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controller/app.controller';
+import { AppService } from './services/app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Iot, IotSchema } from './schemas/iot.schema';
+
+const mongoUrl: string =
+  process.env.MONGO_DB || 'mongodb://localhost:27017/iot-dev';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(mongoUrl),
+    MongooseModule.forFeature([{ name: Iot.name, schema: IotSchema }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
