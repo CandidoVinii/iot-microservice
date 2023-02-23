26db42b6ce50814fc2e91ea1952c4dbea09207d4
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-02-23 09:55:27 -0300

    feat: add rabbitServiec in module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { IotController } from './controller/iot.controller';
 import { IotService } from './services/iot.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Iot, IotSchema } from './schemas/iot.schema';
+import { RabbitmqService } from './services/rabbitmq.service';
+import { RabbitMQModule } from '@nestjs-plus/rabbitmq';
 
 const mongoUrl: string =
   process.env.MONGO_DB || 'mongodb://localhost:27017/iot-dev';
@@ -13,6 +15,6 @@ const mongoUrl: string =
     MongooseModule.forFeature([{ name: Iot.name, schema: IotSchema }]),
   ],
   controllers: [IotController],
-  providers: [IotService],
+  providers: [IotService, RabbitmqService],
 })
 export class AppModule {}
