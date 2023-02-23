a37f7355240a81d2062a8f138124090824918ff3
CandidoVinii <vinicius.dias2020@engenharia.ufjf.br>
2023-02-23 09:56:24 -0300

    feat: change service in the /iot post for the send message and before create iot device

--- src/controller/iot.controller.ts
+++ src/controller/iot.controller.ts
@@ -1,14 +1,19 @@
+import { RabbitmqService } from './../services/rabbitmq.service';
 import { IotService } from './../services/iot.service';
 import { Iot } from '../schemas/iot.schema';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller()
 export class IotController {
-  constructor(private readonly iotService: IotService) {}
+  constructor(
+    private readonly iotService: IotService,
+    private readonly rabbitmqService: RabbitmqService,
+  ) {}
 
   @Post('/iot')
-  createNewDevice(@Body() iot: Iot) {
-    return this.iotService.create(iot);
+  async createNewDevice(@Body() iot: Iot) {
+    await this.rabbitmqService.startQueue(iot);
+    return 'Iot Cadastrado';
   }
 
   @Get('/iot')
